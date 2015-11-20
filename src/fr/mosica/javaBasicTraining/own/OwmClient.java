9416f355d00d33b8868328ff989cd74fca951a94
Florent FREMONT <ff.fremont.florent@gmail.com>
2015-11-20 09:42:54 +0100

    import

---
+++ src/fr/mosica/javaBasicTraining/own/OwmClient.java
@@ -0,0 +1,26 @@
+package fr.mosica.javaBasicTraining.own;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author florent
+ */
+public class OwmClient {
+    
+    /**
+     * URL du serveur
+     */
+    private String url;
+    
+    private ObjectMapper jsonMapper;
+    
+    
+}
